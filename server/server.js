548b17dfe715b378100046f327073be7150004c9
Psalms91-50-23 <khothach@gmail.com>
2021-09-23 13:52:26 +1200

    add wild card incase routes from front end goes to unknown route, takes them back to home page index

--- server/server.js
+++ server/server.js
@@ -11,4 +11,10 @@ server.use(express.static(path.join(__dirname, 'public')))
 
 
 server.use('/api/v1/barventory', barventoryRoutes)
+
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  })
+  
+
 module.exports = server
