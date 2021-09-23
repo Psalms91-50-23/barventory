392dccd5ea33e028e139110f6cd04e4b4426abbb
Psalms91-50-23 <khothach@gmail.com>
2021-09-23 15:16:49 +1200

    route done for components

---
+++ client/components/BottleSlider.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import {Route} from 'react-router-dom'
+
+function BottleSlider () {
+
+  return (
+    <>
+      <h1>Bottle Slider</h1>
+    </>
+  )
+}
+
+export default BottleSlider
---
+++ client/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import {Route} from 'react-router-dom'
+
+function Home () {
+
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  )
+}
+
+export default Home
