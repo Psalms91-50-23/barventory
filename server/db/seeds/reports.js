7a71760f1dc2fa54819a739273b43dd27761577e
tim-soikie <85976318+tim-soikie@users.noreply.github.com>
2021-09-23 16:12:51 +1200

    seeds + public/img folder for image resources

---
+++ server/db/seeds/reports.js
@@ -0,0 +1,8 @@
+exports.seed = (knex) => {
+  return knex('reports').del()
+    .then(() => {
+      return knex('reports').insert([
+        { id: 1, date: '2021-9-29T00:00:00.000Z', users_id: 1, report_data: [{}] }
+      ])
+    })
+}
