19bde0414d7af300d98201cc11f3ac0c772e5276
tim-soikie <85976318+tim-soikie@users.noreply.github.com>
2021-09-24 17:33:09 +1200

    fixed inventory table column for sizes

--- server/db/migrations/20210923150456_inventory.js
+++ server/db/migrations/20210923150456_inventory.js
@@ -2,7 +2,8 @@ exports.up = (knex) => {
     return knex.schema.createTable('inventory', table => {
       table.increments('id').primary()
       table.string('name')
-      table.integer('size')
+      table.string('size')
+      table.integer('size_num')
       table.string('image')
       table.string('silhouette')
     })
