97a98edf2c2c12ccaffe1bad9539167a2b642a75
tim-soikie <85976318+tim-soikie@users.noreply.github.com>
2021-09-24 14:15:03 +1200

    seeded scraped data for bottles table

--- server/db/migrations/20210923150447_bottles.js
+++ server/db/migrations/20210923150447_bottles.js
@@ -2,7 +2,8 @@ exports.up = (knex) => {
     return knex.schema.createTable('bottles', table => {
       table.increments('id').primary()
       table.string('name')
-      table.integer('size')
+      table.string('size')
+      table.integer('size_num')
       table.string('image')
       table.string('silhouette')
     })
