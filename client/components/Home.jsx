5e951bea375d3833258a105e9bba97dbab602fdc
alexthomsonnz <alexthomsonnz@gmail.com>
2021-09-27 13:41:50 +1300

    Home/Login + CSS

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,15 +1,60 @@
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
-import {Route} from 'react-router-dom'
-import { fetchInventory  } from '../redux/inventory'
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import PageHeader from "./PageHeader";
 
-function Home (props) {
+function Home(props) {
+  const [showPopup, setShowPopup] = useState(false);
+
+  //Login or Register
+  const [login, setLogin] = useState(true);
+
+  const showLogin = (e) => {
+    setShowPopup(true);
+  };
+  
+  const showRegister = (e) => {
+    setLogin(false);
+    setShowPopup(true);
+  };
+
+  const getTitle = () => {
+    return login ? "Log In" : "Sign Up";
+  }
+
+  function formSubmit(e) {
+    console.log(props)
+    props.history.push("/inventory");
+  }
 
   return (
     <>
-      <h1>Home</h1>
+      <div className="homescreen">
+        <img src="/img/barventory-logo.png" />
+        <button className="login" onClick={showLogin}>
+          Log in
+        </button>
+        <button className="sign-up" onClick={showRegister}>
+          or Sign Up
+        </button>
+
+        <div className={"popup" + (showPopup ? " active" : "")}>
+          <div className="popup-bg" onClick={() => setShowPopup(false)}></div>
+          <div className="popup-content">
+            <PageHeader
+              title={getTitle()}
+              leftAction={
+                <button onClick={() => setShowPopup(false)}>Cancel</button>
+              }
+            />
+            {login && <input name="name" placeholder="Name" />}
+            <input name="email" placeholder="Username" />
+            <input name="password" placeholder="Password" />
+            <button class="submit" onClick={formSubmit}>{getTitle()}</button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default connect() (Home)
+export default connect()(Home);
