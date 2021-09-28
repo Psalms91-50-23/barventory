9a053d71290b16f93d5850c3ce6aff404428bfda
alexthomsonnz <alexthomsonnz@gmail.com>
2021-09-28 15:53:19 +1300

    Empty & Loading screens

--- client/components/InventoryScreen.jsx
+++ client/components/InventoryScreen.jsx
@@ -4,6 +4,7 @@ import { fetchInventory } from "../redux/inventory";
 import Table from "./Table";
 import { NavLink, useHistory } from "react-router-dom";
 import PageHeader from "./PageHeader";
+import Loading from "./Loading";
 
 function InventoryScreen(props) {
   const { dispatch, inventoryState } = props;
@@ -23,20 +24,25 @@ function InventoryScreen(props) {
           </NavLink>
         }
       />
-      <div>
-        {inventoryState.inventory.length > 0 && (
-          <Table key={"table"} inventory={inventoryState.inventory} />
-        )}
-      </div>
-      {inventoryState.inventory.length == 0 && (
-        <div class="empty">
-          <h1 class="text-primary">Inventory empty 🍺</h1>
-          <p>To Get Started, add some items to your inventory</p>
-          <NavLink className="button" to="/addBottle">
-            Add Bottles
-          </NavLink>
+      {!inventoryState.loading && (
+        <>
+        <div>
+          {inventoryState.inventory.length > 0 && (
+            <Table key={"table"} inventory={inventoryState.inventory} />
+          )}
         </div>
+        {inventoryState.inventory.length == 0 && (
+          <div className="empty">
+            <h1 className="text-primary">Inventory empty 🍺</h1>
+            <p>To Get Started, add some items to your inventory</p>
+            <NavLink className="button" to="/addBottle">
+              Add Bottles
+            </NavLink>
+          </div>
+        )}
+        </>
       )}
+      {inventoryState.loading && <Loading />}
     </>
   );
 }
